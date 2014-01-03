aa05f226ad77dd5c29d40d7946e33bdc221b19e9
xxSlayeRxx <xxsl4y3rxx@gmail.com>
2014-01-04 00:55:27 +0100

    Embedded dlls

--- Src/OtakuHelperHelperApp/OHHelper/App.xaml.cs
+++ Src/OtakuHelperHelperApp/OHHelper/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,5 +17,31 @@ namespace OHHelper
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+
+                Assembly thisAssembly = Assembly.GetExecutingAssembly();
+
+                //Get the Name of the AssemblyFile
+                var name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+
+                //Load form Embedded Resources - This Function is not called if the Assembly is in the Application Folder
+                var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
+                if (resources.Count() > 0)
+                {
+                    var resourceName = resources.First();
+                    using (Stream stream = thisAssembly.GetManifestResourceStream(resourceName))
+                    {
+                        if (stream == null) return null;
+                        var block = new byte[stream.Length];
+                        stream.Read(block, 0, block.Length);
+                        return Assembly.Load(block);
+                    }
+                }
+                return null;
+            };
+        }
     }
 }
